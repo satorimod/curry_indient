a17744bf322fc5250405808340b6f117fbec45e5
satorimod <74944697+satorimod@users.noreply.github.com>
2021-01-14 19:02:55 +0900

    バリデーションの記述

--- app/models/gourmet.rb
+++ app/models/gourmet.rb
@@ -1,2 +1,16 @@
 class Gourmet < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :feature
+    validates :content
+
+    with_options numericality: { other_than: 1 } do
+      validates :genre_id
+      validates :area_id
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :gourmets
+  has_many :comments
+
   with_options presence: true do
     validates :last_name
     validates :first_name
