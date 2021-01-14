18ce266650d5abe08a475f523f823fc272b33d3c
satorimod <74944697+satorimod@users.noreply.github.com>
2021-01-14 11:46:07 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :date_of_birth
+    validates :nickname
+  end           
 end
