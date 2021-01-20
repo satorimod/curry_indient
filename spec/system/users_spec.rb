04fd30d3d53725a18137c34b57f77eb82b9a5a0c
satorimod <74944697+satorimod@users.noreply.github.com>
2021-01-20 16:15:19 +0900

    結合テストコード途中

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('Sign up')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移する
+      expect(current_path).to eq root_path
+      # ログイン中のトップページにログアウトボタンが表示されることを確認する
+      expect(page).to have_content('Log out')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('Sign up')
+      expect(page).to have_no_content('Log in')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      # 新規登録ページへ移動する
+      # ユーザー情報を入力する
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      # 新規登録ページへ戻されることを確認する
+    end
+  end
+end
