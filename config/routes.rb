ffb7e3a8111aacbe683259361696a9f86b6b9b4a
satorimod <74944697+satorimod@users.noreply.github.com>
2021-02-18 18:32:49 +0900

    投稿検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   }
   root to: 'gourmets#index'
   resources :gourmets do
-    resources :comments, only: :create
+    collection do
+      get 'search'
+      resources :comments, only: :create
+    end  
   end  
 end
